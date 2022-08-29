8156955cc897bf8b8ba32a7f7ddce0ed795d96c9
Kiefer Weis <kieferweis@protonmail.com>
2022-08-29 17:38:32 -0400

    set up button/function reset timer/instructions

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@
 document.getElementById("beginButton").addEventListener("click", breathCounting)
 document.getElementById("stopButton").addEventListener("click", reset)
 
+let seconds = 0;
 
 // Function that pushes the count to the counting area (fires on clicking Begin button)
 function breathCounting() {
-  let seconds = 0;
 
+
+}
+
+// Function to reset the process
+function reset() {
+  seconds = 0;
+  document.getElementById("instructionArea").innerHTML = "";
+  document.getElementById("secondCountArea").innerHTML = "";
 }
