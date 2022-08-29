645585f14885c1965b7428e6406cfe1c42c7cfec
Kiefer Weis <kieferweis@protonmail.com>
2022-08-29 17:33:56 -0400

    first commit with basic webpage structure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Event listener for the buttons
+document.getElementById("beginButton").addEventListener("click", breathCounting)
+document.getElementById("stopButton").addEventListener("click", reset)
+
+
+// Function that pushes the count to the counting area (fires on clicking Begin button)
+function breathCounting() {
+  let seconds = 0;
+
+}
