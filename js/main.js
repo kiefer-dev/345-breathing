a9f98fceaa27896ddadb631e3dcb1071436188df
Kiefer Weis <kieferweis@protonmail.com>
2022-08-29 17:49:48 -0400

    add logic to alternate start/stop buttons and clear interval on stop

--- js/main.js
+++ js/main.js
@@ -3,16 +3,30 @@ document.getElementById("beginButton").addEventListener("click", breathCounting)
 document.getElementById("stopButton").addEventListener("click", reset)
 
 let seconds = 0;
+let timerVariable;
+
+function countSeconds() {
+  ++seconds;
+  document.getElementById("secondCountArea").innerHTML = seconds;
+}
 
 // Function that pushes the count to the counting area (fires on clicking Begin button)
 function breathCounting() {
+  document.getElementById("beginButton").classList.toggle("hidden");
+  document.getElementById("stopButton").classList.toggle("hidden");
+
+  timerVariable = setInterval(countSeconds, 1000);
+
 
 
 }
 
 // Function to reset the process
 function reset() {
+  document.getElementById("stopButton").classList.toggle("hidden");
+  document.getElementById("beginButton").classList.toggle("hidden");
   seconds = 0;
+  clearInterval(timerVariable);
   document.getElementById("instructionArea").innerHTML = "";
   document.getElementById("secondCountArea").innerHTML = "";
 }
