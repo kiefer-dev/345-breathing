c212f1db0930c5e082e9738902699de921e7f38e
Kiefer Weis <kieferweis@protonmail.com>
2022-09-09 13:32:46 -0400

    Add basic settings section, updated main.js logic for viewing settings

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-document.querySelector('.aboutButton').addEventListener('click', showAboutText);
-
+document.getElementById('aboutButton').addEventListener('click', showAboutText);
 function showAboutText() {
   document.querySelector('.about-text').classList.toggle('hidden');
+
+  if (!document.querySelector('.settings-section').classList.contains('hidden')) {
+    document.querySelector('.settings-section').classList.toggle('hidden');
+  }
+}
+
+document.getElementById('settingsButton').addEventListener('click', showSettings);
+function showSettings() {
+  document.querySelector('.settings-section').classList.toggle('hidden');
+
+  if (!document.querySelector('.about-text').classList.contains('hidden')) {
+    document.querySelector('.about-text').classList.toggle('hidden');
+  }
 }
