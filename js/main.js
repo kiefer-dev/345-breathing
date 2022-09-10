b918b04152e4b6e1510992d6a9bfea8a0364632c
Kiefer Weis <kieferweis@protonmail.com>
2022-09-10 13:13:08 -0400

    Setup logic for toggling nightmode using settings checkbox

--- js/main.js
+++ js/main.js
@@ -15,3 +15,17 @@ function showSettings() {
     document.querySelector('.about-text').classList.toggle('hidden');
   }
 }
+
+document.getElementById('darkModeCheckbox').addEventListener('change', toggleDarkMode);
+function toggleDarkMode() {
+  if (document.getElementById('darkModeCheckbox').checked) {
+    document.querySelector('body').classList.toggle('light-mode');
+    document.querySelector('body').classList.toggle('dark-mode');
+  } else {
+    document.querySelector('body').classList.toggle('light-mode');
+    document.querySelector('body').classList.toggle('dark-mode');
+  }
+}
+
+
+// Todo: set up local storage to remember user's settings
